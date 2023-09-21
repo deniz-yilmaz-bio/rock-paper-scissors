8119b3626de6df39c21dc9bbc174b8a968125401
deniz-yilmaz-bio <bora.yilmaz@ug.bilkent.edu.tr>
2023-09-21 22:10:53 +0300

    Create game function for one round if the player picks rock.

--- gamecode.js
+++ gamecode.js
@@ -5,3 +5,18 @@ function getComputerChoice() {
 
   return choiceArray[randomNumber];
 }
+
+function playRound(userChoice, computerChoice) {
+  computerChoice = getComputerChoice();
+  userChoice = prompt("Make your choice:");
+
+  if (userChoice.toLowerCase() == "rock") {
+    if (computerChoice === "Paper") {
+      return "You lose! " + computerChoice + " beats Rock!";
+    } else if (computerChoice == "Scissors") {
+      return "You win! Rock beats " + computerChoice;
+    } else {
+      return "It's a draw!";
+    }
+  }
+}
