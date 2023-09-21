2281d229ac65ed0d5837a7be98ddc4e902610dc9
deniz-yilmaz-bio <bora.yilmaz@ug.bilkent.edu.tr>
2023-09-21 22:25:37 +0300

    Finalize one round function.

--- gamecode.js
+++ gamecode.js
@@ -18,5 +18,23 @@ function playRound(userChoice, computerChoice) {
     } else {
       return "It's a draw!";
     }
+  } else if (userChoice.toLowerCase() == "paper") {
+    if (computerChoice === "Scissors") {
+      return "You lose! " + computerChoice + " beats Paper!";
+    } else if (computerChoice == "Rock") {
+      return "You win! Paper beats " + computerChoice;
+    } else {
+      return "It's a draw!";
+    }
+  } else if (userChoice.toLowerCase() == "scissors") {
+    if (computerChoice === "Rock") {
+      return "You lose! " + computerChoice + " beats Scissors!";
+    } else if (computerChoice == "Paper") {
+      return "You win! Scissors beats " + computerChoice;
+    } else {
+      return "It's a draw!";
+    }
+  } else {
+    return "Invalid Choice.";
   }
 }
